8207b4debfeff859a38c5f22d207b9a7e90f057c
kazimorwan498 <kazimdmorwan498@gmail.com>
2024-11-06 23:53:11 +0600

    retried to solve the javascript not working problem.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                main: resolve(__dirname, "./src/index.js"),
+                mainJs: resolve(__dirname, "./src/index.js"),
             },
         },
     },
